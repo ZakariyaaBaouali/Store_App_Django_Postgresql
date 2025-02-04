99fc1c1078bc528bb121294e220c34503871e164
zakaria <zakariabaouali@outlook.com>
2025-02-04 12:47:04 +0100

    implement the Order model and the Order Items model with many-to-many relationship between the Order and the Product models

--- store/models.py
+++ store/models.py
@@ -36,4 +36,28 @@ class Review(models.Model):
 
 
 
+class Order(models.Model):
+    ORDER_STATUS_PENDING = 'P'
+    ORDER_STATUS_COMPLETE = 'C'
+    ORDER_STATUS_FAILED = 'F'
+
+    ORDER_STATUS = [
+        (ORDER_STATUS_PENDING , 'pending'),
+        (ORDER_STATUS_COMPLETE , 'complete'),
+        (ORDER_STATUS_FAILED , 'failed')
+    ]
+
+    user = models.ForeignKey(User , on_delete=models.SET_NULL , related_name="user_orders")
+    order_total_amount = models.DecimalField(max_digits=6 , decimal_places=2)
+    order_status = models.CharField(max_length=1 , choices=ORDER_STATUS , default=ORDER_STATUS_PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+class OrderItems(models.Model):
+    order = models.ForeignKey(Order , on_delete=models.PROTECT , related_name="order_items")
+    product = models.ForeignKey(Product , on_delete=models.SET_NULL)
+    order_item_quantity = models.SmallIntegerField()
+    order_item_price = models.DecimalField(max_digits=6 , decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
 
