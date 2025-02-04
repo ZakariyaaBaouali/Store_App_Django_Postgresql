6e3f89d2f64a5fc10f7f880693e2491a0f157d94
zakaria <zakariabaouali@outlook.com>
2025-02-04 12:58:17 +0100

    implement the Wishlist model with one-to-one relationship with the User and the Product models

--- store/models.py
+++ store/models.py
@@ -61,3 +61,11 @@ class OrderItems(models.Model):
     order_item_price = models.DecimalField(max_digits=6 , decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
+
+class Wishlist(models.Model):
+    user = models.OneToOneField(User , on_delete=models.CASCADE , primary_key=True)
+    product = models.OneToOneField(Product , on_delete=models.CASCADE , primary_key=True)
+    product_quantity = models.DecimalField(max_digits=6 , decimal_places=2)
+    added_at = models.DateTimeField(auto_now_add=True)
+
+
