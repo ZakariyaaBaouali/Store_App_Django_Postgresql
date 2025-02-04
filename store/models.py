a606accf5cc7ed9675e5fd4d18bc1cf1e3745eb7
zakaria <zakariabaouali@outlook.com>
2025-02-04 12:27:47 +0100

    implement the Collection model with a one-to-many relationship with the Product model

--- store/models.py
+++ store/models.py
@@ -9,3 +9,19 @@ class User(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
+class Collection(models.Model):
+    collection_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=255)
+    product_description = models.TextField()
+    product_price = models.DecimalField(max_digits=6 , decimal_places=2)
+    product_stock_quantity = models.SmallIntegerField(default=0)
+    product_category = models.ForeignKey(Collection , on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
