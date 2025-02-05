d78de60ddc286639a3409616a2f5d8b664b794f3
zakaria <zakariabaouali@outlook.com>
2025-02-05 13:45:27 +0100

    fix Wishlist model in order to create many-to-many relationship between the User Model and the Product model

--- store/models.py
+++ store/models.py
@@ -63,7 +63,7 @@ class OrderItems(models.Model):
 
 
 class Wishlist(models.Model):
-    user = models.OneToOneField(User , on_delete=models.CASCADE , primary_key=True)
+    user = models.OneToOneField(User , on_delete=models.CASCADE)
     product = models.OneToOneField(Product , on_delete=models.CASCADE)
     product_quantity = models.DecimalField(max_digits=6 , decimal_places=2)
     added_at = models.DateTimeField(auto_now_add=True)
