be83811260dcc6fe4435bec69f6d5b8e5dc4224c
zakaria <zakariabaouali@outlook.com>
2025-02-05 14:59:49 +0100

    create new migration

--- store/models.py
+++ store/models.py
@@ -63,7 +63,7 @@ class OrderItems(models.Model):
 
 
 class Wishlist(models.Model):
-    user = models.ForeignKey(User , on_delete=models.CASCADE)
+    user = models.ForeignKey(User , on_delete=models.CASCADE , related_name="user_wishlist")
     product = models.ForeignKey(Product , on_delete=models.CASCADE)
     product_quantity = models.DecimalField(max_digits=6 , decimal_places=2)
     added_at = models.DateTimeField(auto_now_add=True)
