543abd30ce88e4c9386e07aeaa1bd4e9e4f420dc
zakaria <zakariabaouali@outlook.com>
2025-02-06 12:33:10 +0100

    setup product details end point

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-     path('products/' , views.product_list)
+    path('products/', views.product_list),
+    path('products/<id>/', views.product_detail)
 ]
