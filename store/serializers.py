f41e6ffbd90cc9a85991e42e5d44cd961cb56de6
zakaria <zakariabaouali@outlook.com>
2025-02-06 23:03:37 +0100

    fix Serializers inheritance

--- store/serializers.py
+++ store/serializers.py
@@ -1,56 +1,56 @@
 from rest_framework import serializers
-from models import Product, User, Collection, Order, Review, OrderItems, Discount, OrderDiscount, Wishlist
+from .models import Product, User, Collection, Order, Review, OrderItems, Discount, OrderDiscount, Wishlist
 
 
-class ProductSerializer(serializers.Serializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
 
-class UserSerializer(serializers.Serializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'user_name', 'user_email', 'user_profile_url', 'created_at', 'updated_at']
 
 
-class CollectionSerializer(serializers.Serializer):
+class CollectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Collection
         fields = '__all__'
 
 
-class OrderSerializer(serializers.Serializer):
+class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
 
 
-class ReviewSerializer(serializers.Serializer):
+class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
 
 
-class OrderItemsSerializer(serializers.Serializer):
+class OrderItemsSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItems
         fields = '__all__'
 
 
-class DiscountSerialize(serializers.Serializer):
+class DiscountSerialize(serializers.ModelSerializer):
     class Meta:
         model = Discount
         fields = '__all__'
 
 
-class OrderDiscountSerializer(serializers.Serializer):
+class OrderDiscountSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderDiscount
         fields = '__all__'
 
 
-class WishlistSerializer(serializers.Serializer):
+class WishlistSerializer(serializers.ModelSerializer):
     class Meta:
         model = Wishlist
         fields = '__all__'
