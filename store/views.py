96b3ac25f8c0a025d59f5bbc6bc2307c5441217b
zakaria <zakariabaouali@outlook.com>
2025-02-06 23:41:14 +0100

    setup the base structure for an API end points -- just starting 🙌

--- store/views.py
+++ store/views.py
@@ -1,11 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product , User
+from .serializers import ProductSerializer , UserSerializer
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
-@api_view()
+@api_view(["GET"])
 def product_list(request):
-    return Response('ok')
+    query_set = Product.objects.all()
+    serializer = UserSerializer(query_set , many=True)
+    print(serializer.data)
+    return Response(serializer.data)
 
-@api_view()
+@api_view(["GET"])
 def product_detail(request , id):
-    return Response(f"product detail for id : {id}")
+    product = get_object_or_404(Product ,  pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
