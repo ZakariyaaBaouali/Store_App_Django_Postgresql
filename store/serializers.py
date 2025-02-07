07a5296841dcd75d6471162e4ddac59c22064c37
zakaria <zakariabaouali@outlook.com>
2025-02-07 12:14:24 +0100

    enhance the Product Serializer, so we can retrieve also the collection data 😏 , and change User_name to full_name in User Serialier 🙌

--- store/serializers.py
+++ store/serializers.py
@@ -3,27 +3,29 @@ from rest_framework import serializers
 from .models import Product, User, Collection, Order, Review, OrderItems, Discount, OrderDiscount, Wishlist
 
 
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = '__all__'
+
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
     product_tax = serializers.SerializerMethodField(method_name='calculate_tax')
+    product_category = CollectionSerializer()
 
     def calculate_tax(self, product: Product):
         return product.product_price * Decimal(1.1)
 
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source='user_name')
+
     class Meta:
         model = User
         fields = ['id', 'user_name', 'user_email', 'user_profile_url', 'created_at', 'updated_at']
-
-
-class CollectionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Collection
-        fields = '__all__'
 
 
 class OrderSerializer(serializers.ModelSerializer):
